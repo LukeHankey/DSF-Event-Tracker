735540b7fcd662e3fc326f376ebc9fa75e954150
Luke Hankey <l.hankey@oxcan.org>
2025-03-13 12:43:38 +0000

    Fix TS types

---
+++ src/types/alt1.d.ts
@@ -0,0 +1,21 @@
+declare module "alt1/fonts/aa_8px_mono.js" {
+    interface FontChar {
+        width: number;
+        bonus: number;
+        chr: string;
+        pixels: number[];
+        secondary: boolean;
+    }
+
+    interface FontMetadata {
+        chars: FontChar[];
+        width: number;
+        spacewidth: number;
+        shadow: boolean;
+        height: number;
+        basey: number;
+    }
+
+    const font: FontMetadata;
+    export default font;
+}
