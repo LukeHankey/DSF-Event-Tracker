ff454d92baa929b2c7ee1401f196045cc4eb5553
Luke Hankey <l.hankey@oxcan.org>
2025-05-02 23:07:17 +0100

    updates

--- alt1/config.ts
+++ alt1/config.ts
@@ -1,3 +1,3 @@
-export const DEBUG = true;
+export const DEBUG = false;
 export const ORIGIN = DEBUG ? document.location.href : "https://www.dsfeventtracker.com/alt1";
 export const API_URL = DEBUG ? "http:localhost:8000" : "https://api.dsfeventtracker.com";
