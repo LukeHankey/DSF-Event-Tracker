851f2fd37854b6c2c8239d5445842749960fb991
Luke <Lukehankey@gmail.com>
2025-05-23 22:18:02 +0100

    Add appconfig back into the build directories (#91)

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,10 @@ module.exports = (env = {}) => {
             new CopyWebpackPlugin({
                 patterns: [
                     {
+                        from: path.resolve(__dirname, "alt1/appconfig.json"),
+                        to: ".",
+                    },
+                    {
                         from: path.resolve(__dirname, "alt1/assets"),
                         to: "assets",
                     },
