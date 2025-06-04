fbee2c6994b5b948620dac8f8e02d0548bd2cd0f
Alex Kempf <alex.kempf10@gmail.com>
2025-06-03 21:19:16 -0500

    Undo config change

--- alt1/config.ts
+++ alt1/config.ts
@@ -1,3 +1,3 @@
 export const DEBUG = false;
 export const ORIGIN = DEBUG ? document.location.href : "https://www.dsfeventtracker.com/alt1";
-export const API_URL = DEBUG ? "http://localhost:8000" : "http://localhost:3000";
+export const API_URL = DEBUG ? "http://localhost:8000" : "https://api.dsfeventtracker.com";
